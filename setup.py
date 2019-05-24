a1e35c44c1930558550bf187756bfa93470935ad
Robin Ramael <robin.ramael@maykinmedia.nl>
2019-05-24 14:45:22 +0200

    release to pypi

--- setup.py
+++ setup.py
@@ -42,13 +42,12 @@ if sys.argv[-1] == 'tag':
     sys.exit()
 
 readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django-list-tests',
     version=version,
     description="""Add a django management command to list all tests in the project""",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author='Robin Ramael',
     author_email='robin.ramael@gmail.com',
     url='https://github.com/robinramael/django-list-tests',
