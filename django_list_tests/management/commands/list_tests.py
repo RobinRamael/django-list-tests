a3db189d88b378178b2806e7c042764918104ebf
Robin Ramael <robin.ramael@maykinmedia.nl>
2019-05-28 14:45:59 +0200

    also list test modules and add flag to turn this off

--- django_list_tests/management/commands/list_tests.py
+++ django_list_tests/management/commands/list_tests.py
@@ -3,22 +3,38 @@ import unittest
 from django.core.management import BaseCommand
 
 
-def print_suite(suite, stdout):
+def get_test_names(suite, base_app_name, methods_only=False):
+
+    test_names = set()
+
     if hasattr(suite, "__iter__"):
         for x in suite:
-            print_suite(x, stdout)
+            test_names |= get_test_names(x, base_app_name, methods_only)
     else:
-        stdout.write(
-            "{mod}.{klass}.{method}".format(
-                mod=suite.__module__, klass=suite.__class__.__name__, method=suite._testMethodName
-            )
+        method = "{mod}.{klass}.{method}".format(
+            mod=suite.__module__, klass=suite.__class__.__name__, method=suite._testMethodName
         )
+        klass = "{mod}.{klass}".format(mod=suite.__module__, klass=suite.__class__.__name__)
+
+        test_names.add(method)
+        if not methods_only:
+            test_names.add(klass)
+            test_names.add(suite.__module__)
+
+    return test_names
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("app_name", default=".", nargs="?")
+        parser.add_argument("--methods-only", dest="methods_only", action="store_true")
 
     def handle(self, *args, **kwargs):
         app_name = kwargs.get("app_name", ".")
-        print_suite(unittest.defaultTestLoader.discover(app_name), self.stdout)
+        methods_only = kwargs.get("methods_only", False)
+        suite = unittest.defaultTestLoader.discover(app_name)
+
+        test_names = get_test_names(suite, app_name, methods_only)
+
+        for name in test_names:
+            self.stdout.write(name)
