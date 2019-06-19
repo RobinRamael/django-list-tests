aa7e85481b7263f6a7b7c3008bd59dcb4f9b1589
Robin Ramael <robin.ramael@maykinmedia.nl>
2019-06-19 10:13:08 +0200

    live reload with watchdog

--- django_list_tests/management/commands/list_tests.py
+++ django_list_tests/management/commands/list_tests.py
@@ -1,7 +1,10 @@
-import os
+import time
 import unittest
 
 from django.core.management import BaseCommand
+from livereload import Server
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from django_list_tests.common import load_mru_file
 
@@ -27,33 +30,72 @@ def get_test_names(suite, base_app_name, methods_only=False):
     return test_names
 
 
+def get_sorted_test_names(app_name, use_mru=True, methods_only=False):
+    suite = unittest.defaultTestLoader.discover(app_name)
+
+    test_names = get_test_names(suite, app_name, methods_only)
+
+    if use_mru:
+        mru_tests = load_mru_file()
+        unused_tests = test_names - set(mru_tests.keys())
+        ordered_names = [test for test, _ in mru_tests.most_common()] + sorted(unused_tests)
+    else:
+        ordered_names = sorted(test_names)
+
+    return ordered_names
+
+
+def write_names(names, fp):
+    fp.writelines("{}\n".format(name) for name in names)
+
+
+def list_tests(app_name, use_mru=True, methods_only=False, outfile=None, stdout=None):
+    test_names = get_sorted_test_names(app_name, use_mru, methods_only)
+
+    if outfile:
+        with open(outfile, "w") as fp:
+            write_names(test_names, fp)
+    else:
+        write_names(test_names, stdout)
+
+
+class ListTestsHandler(FileSystemEventHandler):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def on_modified(self, event):
+        print("change detected, regenerating test names")
+        list_tests(*self.args, **self.kwargs)
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("app_name", default=".", nargs="?")
         parser.add_argument("--methods-only", dest="methods_only", action="store_true")
         parser.add_argument("--no-mru", dest="use_mru", action="store_false")
         parser.add_argument("--out", dest="outfile", default=None)
+        parser.add_argument("--watch", action="store_true", dest="watch", default=None)
 
     def handle(self, *args, **kwargs):
         app_name = kwargs.get("app_name", ".")
         methods_only = kwargs.get("methods_only", False)
         outfile = kwargs.get("outfile", None)
         use_mru = kwargs.get("use_mru", True)
+        watch = kwargs.get("watch", False)
 
-        suite = unittest.defaultTestLoader.discover(app_name)
+        if not watch:
+            return list_tests(app_name, use_mru, methods_only, outfile, self.stdout)
 
-        test_names = get_test_names(suite, app_name, methods_only)
+        observer = Observer()
 
-        if use_mru:
-            mru_tests = load_mru_file()
-            unused_tests = test_names - set(mru_tests.keys())
-            ordered_names = [test for test, _ in mru_tests.most_common()] + sorted(unused_tests)
-        else:
-            ordered_names = sorted(test_names)
+        handler = ListTestsHandler(app_name, use_mru, methods_only, outfile, self.stdout)
+        observer.schedule(handler, "src/", recursive=True)
+        observer.start()
 
-        if outfile:
-            with open(outfile, "w") as fp:
-                fp.writelines("{}\n".format(name) for name in ordered_names)
-        else:
-            for name in ordered_names:
-                self.stdout.write(name)
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
